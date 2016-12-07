976851abee113ceb1c687b7a17e68bd9e6d30d38
Luke <lukeklepfer@gmail.com>
2016-12-07 13:07:16 -0600

    Auth Done
    Auth with completion and error handling.

--- ClapChat/AuthService.swift
+++ ClapChat/AuthService.swift
@@ -10,6 +10,8 @@ import Foundation
 import FirebaseAuth
 
 
+typealias Completion = (_ errMSg: String?, _ data: Any?) -> Void
+
 class AuthService {
     private static let _instance = AuthService()
     
@@ -17,7 +19,74 @@ class AuthService {
         return _instance
     }
     
-    func login(email: String, pass: String){
+    func login(email: String, pass: String, onComplete: Completion?){
         
+        FIRAuth.auth()?.signIn(withEmail: email, password: pass, completion: { (user, error) in
+            
+            if error != nil {
+                if let errorCode = FIRAuthErrorCode(rawValue: error!._code){
+                    if errorCode == FIRAuthErrorCode.errorCodeUserNotFound{
+                        FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: { (user, error) in
+                            if error != nil{
+                                self.handleFireBaseError(error: error! as NSError, onComplete: onComplete)
+                            }else{
+                                //user created
+                                if user?.uid != nil{
+                                    FIRAuth.auth()?.signIn(withEmail: email, password: pass, completion: { (user, error) in
+                                        
+                                        if error != nil{
+                                            //sign in error
+                                            self.handleFireBaseError(error: error! as NSError, onComplete: onComplete)
+                                        }else{
+                                            //logged in
+                                            onComplete?(nil, user)
+                                        }
+                                    })
+                                }
+                            }
+                        })
+                    }
+                    else{
+                        //other errors
+                        self.handleFireBaseError(error: error! as NSError, onComplete: onComplete)
+                    }
+                }
+                
+            }else{
+                //signed in previous user
+                onComplete?(nil, user)
+            }
+        })
     }
+    
+    func handleFireBaseError(error: NSError, onComplete: Completion?){
+        print(error.debugDescription)
+        if let errorCode = FIRAuthErrorCode(rawValue: error._code){
+            switch (errorCode) {
+            case .errorCodeInvalidEmail:
+                onComplete?("Invalid Email Address", nil)
+                break
+            case .errorCodeWrongPassword:
+                onComplete?("Invalid Password", nil)
+                break
+            case .errorCodeEmailAlreadyInUse,
+                .errorCodeAccountExistsWithDifferentCredential:
+                onComplete?("Email already in use", nil)
+                break
+            default:
+                onComplete?("There was a problem Authenticating, try again.", nil)
+                
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- ClapChat/LoginVC.swift
+++ ClapChat/LoginVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var emailTxtField: UITextField!
     @IBOutlet weak var passwordTxtField: UITextField!
@@ -16,6 +16,9 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.emailTxtField.delegate = self
+        self.passwordTxtField.delegate = self
 
     }
 
@@ -23,8 +26,18 @@ class LoginVC: UIViewController {
         
         if let email = emailTxtField.text, let password = passwordTxtField.text, (email.characters.count > 0 && password.characters.count > 0 ){
             
-            AuthService.instance.login(email: email, pass: password)
-            //just checking git
+            AuthService.instance.login(email: email, pass: password, onComplete: { (errMsg, data) in
+                
+                guard errMsg == nil else{
+                    let alert = UIAlertController(title: "Error Authenticating", message: errMsg, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                }
+                
+                self.dismiss(animated: true, completion: nil)
+                
+            })
             
         }else{
             let alert = UIAlertController(title: "Email and Password Required", message: "Enter an email and Password", preferredStyle: .alert)
@@ -33,6 +46,10 @@ class LoginVC: UIViewController {
             
         }
         
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 
 }
